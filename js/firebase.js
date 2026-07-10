03b2aaaec4b61f0f19206a5479756d738e8c8af8
abdullahaljafary3-web <abdullahaljafary3@gmail.com>
2026-07-10 17:31:02 +0300

    Reformat Firebase configuration comments and code

--- js/firebase.js
+++ js/firebase.js
@@ -12,14 +12,15 @@ import {
 
 // بيانات مشروعك
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyC-myHs4l_gK9kMLbuImVQrd7JAJIlTeiY",
-    authDomain: "nasiha-ff5c0.firebaseapp.com",
-    projectId: "nasiha-ff5c0",
-    storageBucket: "nasiha-ff5c0.firebasestorage.app",
-    messagingSenderId: "124502398827",
-    appId: "1:124502398827:web:8ca102d1f2fa8e8efdde2e"
-  };
+ const firebaseConfig = {
+  apiKey: "AIzaSyC-myHs4l_gK9kMLbuImVQrd7JAJIlTeiY",
+  authDomain: "nasiha-ff5c0.firebaseapp.com",
+  projectId: "nasiha-ff5c0",
+  storageBucket: "nasiha-ff5c0.firebasestorage.app",
+  messagingSenderId: "124502398827",
+  appId: "1:124502398827:web:8ca102d1f2fa8e8efdde2e"
+};
+
 
 // تشغيل Firebase
 const app = initializeApp(firebaseConfig);
