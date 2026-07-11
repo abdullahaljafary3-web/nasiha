3fcdba6d15d5a5c797708615f3106854657ba2f0
abdullahaljafary3-web <abdullahaljafary3@gmail.com>
2026-07-11 10:43:17 +0300

    Add article loading functionality with Firestore

---
+++ js/article.js
@@ -0,0 +1,66 @@
+import { db } from "./firebase.js";
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+const params = new URLSearchParams(window.location.search);
+
+const articleId = params.get("id");
+
+const container = document.getElementById("articleContainer");
+
+async function loadArticle(){
+
+    if(!articleId){
+
+        container.innerHTML="<h2>المقال غير موجود</h2>";
+
+        return;
+
+    }
+
+    const ref = doc(db,"articles",articleId);
+
+    const snap = await getDoc(ref);
+
+    if(!snap.exists()){
+
+        container.innerHTML="<h2>المقال غير موجود</h2>";
+
+        return;
+
+    }
+
+    const article = snap.data();
+
+    document.title = article.title;
+
+    container.innerHTML = `
+
+        <img src="${article.image}" class="article-cover">
+
+        <h1>${article.title}</h1>
+
+        <div class="article-meta">
+
+            ${article.category}
+
+            |
+
+            ${article.date}
+
+        </div>
+
+        <div class="article-content">
+
+            ${article.content}
+
+        </div>
+
+    `;
+
+}
+
+loadArticle();
