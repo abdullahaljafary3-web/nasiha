ddf45bbf94323c3d14af8c39a8cd7fb850ffe0fb
abdullahaljafary3-web <abdullahaljafary3@gmail.com>
2026-07-11 17:26:33 +0300

    Update Firebase configuration in auth.js

--- admin/js/auth.js
+++ admin/js/auth.js
@@ -10,20 +10,13 @@ signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
 // استخدم نفس بيانات firebaseConfig الموجودة في js/firebase.js
-const firebaseConfig = {
-
-apiKey:"",
-
-authDomain:"",
-
-projectId:"",
-
-storageBucket:"",
-
-messagingSenderId:"",
-
-appId:""
-
+ const firebaseConfig = {
+  apiKey: "AIzaSyC-myHs4l_gK9kMLbuImVQrd7JAJIlTeiY",
+  authDomain: "nasiha-ff5c0.firebaseapp.com",
+  projectId: "nasiha-ff5c0",
+  storageBucket: "nasiha-ff5c0.firebasestorage.app",
+  messagingSenderId: "124502398827",
+  appId: "1:124502398827:web:8ca102d1f2fa8e8efdde2e"
 };
 
 const app=initializeApp(firebaseConfig);
