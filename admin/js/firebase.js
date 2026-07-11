9c1d91d41c9833f938475e0ec2d707c7b644539b
abdullahaljafary3-web <abdullahaljafary3@gmail.com>
2026-07-11 17:51:27 +0300

    Update firebase.js

--- admin/js/firebase.js
+++ admin/js/firebase.js
@@ -1,1 +1,28 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+const firebaseConfig = {
+
+    apiKey: "ضع نفس القيم الموجودة في js/firebase.js",
+
+    authDomain: "",
+
+    projectId: "",
+
+    storageBucket: "",
+
+    messagingSenderId: "",
+
+    appId: ""
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export const auth = getAuth(app);
