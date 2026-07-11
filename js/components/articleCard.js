746d364a87cc13c0aea68247ec7f6f2977e6f3d6
abdullahaljafary3-web <abdullahaljafary3@gmail.com>
2026-07-11 10:31:57 +0300

    Add createArticleCard function for article display

--- js/components/articleCard.js
+++ js/components/articleCard.js
@@ -1,1 +1,43 @@
+export function createArticleCard(id, article){
 
+    return `
+
+    <article class="article-card">
+
+        <div class="article-image">
+
+            <img
+            src="${article.image || 'images/default-cover.jpg'}"
+            alt="${article.title}">
+
+        </div>
+
+        <div class="article-content">
+
+            <h3>${article.title}</h3>
+
+            <p>${article.summary}</p>
+
+            <div class="article-footer">
+
+                <span>${article.category}</span>
+
+                <span>${article.date}</span>
+
+            </div>
+
+            <a
+            class="btn mt-2"
+            href="article.html?id=${id}">
+
+                اقرأ المقال
+
+            </a>
+
+        </div>
+
+    </article>
+
+    `;
+
+}
