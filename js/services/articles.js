f857128c8e9aa3bbef3fb96e933ce726428c6d36
abdullahaljafary3-web <abdullahaljafary3@gmail.com>
2026-07-11 10:32:49 +0300

    Add getArticles function to fetch articles from Firestore

--- js/services/articles.js
+++ js/services/articles.js
@@ -1,1 +1,23 @@
+import { db } from "../firebase.js";
 
+import {
+
+collection,
+
+getDocs
+
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+export async function getArticles(){
+
+    const snapshot = await getDocs(collection(db,"articles"));
+
+    return snapshot.docs.map(doc=>({
+
+        id:doc.id,
+
+        ...doc.data()
+
+    }));
+
+}
