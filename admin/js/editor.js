e84491097e1087b744ebaff257abda843a7d2258
abdullahaljafary3-web <abdullahaljafary3@gmail.com>
2026-07-12 09:30:37 +0300

    Set up TinyMCE editor configuration
    Initialize TinyMCE editor with Arabic language support and RTL directionality.

--- admin/js/editor.js
+++ admin/js/editor.js
@@ -1,1 +1,35 @@
+tinymce.init({
 
+selector:"#editor",
+
+height:600,
+
+language:"ar",
+
+directionality:"rtl",
+
+menubar:true,
+
+plugins:[
+
+"lists",
+
+"link",
+
+"image",
+
+"table",
+
+"code",
+
+"fullscreen",
+
+"wordcount"
+
+],
+
+toolbar:
+
+"undo redo | styles | bold italic underline | alignleft aligncenter alignright | bullist numlist | link image table | code fullscreen"
+
+});
